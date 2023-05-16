f2ca00bdc415b42c9584a90ca6ce6404c79c75a2
K-Enter777 <msgbox777@yahoo.co.jp>
2023-05-17 06:26:30 +0900

    最初のコンパイラの実装
    最初のコンパイラを実装。
    サイトに倣って、受け取った整数を戻り値とするアセンブラを出力する。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // 引数を取得
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("引数の個数が正しくありません。");
+        return;
+    }
+
+    let num: u8 = match args[1].parse() {
+        Ok(num) => num,
+        Err(_) => {
+            eprintln!("0-255 の範囲の整数を入力してください。");
+            return;
+        }
+    };
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("\tmov rax, {num}");
+    println!("\tret");
 }

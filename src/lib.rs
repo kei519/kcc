c675fc34584760fa5b3063efcff5c4ccc1c546ec
K-Enter777 <msgbox777@yahoo.co.jp>
2024-04-21 22:47:36 +0900

    Add compiloing signle integer

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,16 @@
 /// # Return
 ///
 /// Exit code.
-pub fn main(_args: Vec<String>) -> u8 {
-    println!("Hello Wordl!");
+pub fn main(args: Vec<String>) -> u8 {
+    if args.len() != 2 {
+        eprintln!("Only ONE argument is required.");
+        return 1;
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("  mov rax, {}", args[1].parse::<i32>().unwrap());
+    println!("  ret");
     0
 }

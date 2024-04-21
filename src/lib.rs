4f44360fafd2d474491389a3f8a0ea6ef3aa7e62
K-Enter777 <msgbox777@yahoo.co.jp>
2024-04-21 22:08:34 +0900

    main 関数の lib.rs への分離

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+/// Executes the main logic.
+///
+/// * args - command-line arguments
+///
+/// # Return
+///
+/// Exit code.
+pub fn main(_args: Vec<String>) -> u8 {
+    println!("Hello Wordl!");
+    0
+}

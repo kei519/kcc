74a339011cdc8aa89e10ca4c49a3eccdab3c565a
K-Enter777 <msgbox777@yahoo.co.jp>
2024-04-23 12:27:20 +0900

    Improve error message and add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -22,32 +22,14 @@ pub fn main(args: Vec<String>) -> u8 {
     println!("main:");
 
     let input = args[1].as_str();
-    let tokens = match Tokenizer::new(input).tokenize() {
-        Ok(tokens) => tokens,
+
+    // catch any error
+    match parse(input) {
         Err(e) => {
-            eprintln!("{:?}", e);
+            e.show(input);
             return 1;
         }
-    };
-
-    let mut pos = 0;
-
-    println!("  mov rax, {}", expect_number(&tokens[pos]).unwrap());
-    pos += 1;
-
-    while pos < tokens.len() {
-        if consume(&tokens[pos], '+').is_ok() {
-            pos += 1;
-            // FIXME: This implemntation is not good
-            println!("  add rax, {}", expect_number(&tokens[pos]).unwrap());
-            pos += 1;
-            continue;
-        }
-
-        consume(&tokens[pos], '-').unwrap();
-        pos += 1;
-        println!("  sub rax, {}", expect_number(&tokens[pos]).unwrap());
-        pos += 1;
+        _ => (),
     }
 
     // return code
@@ -56,37 +38,92 @@ pub fn main(args: Vec<String>) -> u8 {
     0
 }
 
-fn expect_number(token: &Token) -> StdResult<u64, ()> {
+/// called by main to catch any error in one place
+fn parse(input: &str) -> Result<()> {
+    let tokens = Tokenizer::new(input).tokenize()?;
+
+    let mut pos = 0;
+
+    println!("  mov rax, {}", expect_number(&tokens[pos])?);
+    pos += 1;
+
+    while pos < tokens.len() {
+        // HACK: I don't think Eof is needed in Rust program.
+        if let TokenKind::Eof = tokens[pos].value {
+            break;
+        }
+
+        if consume(&tokens[pos], '+') {
+            pos += 1;
+            // FIXME: This implemntation is not good
+            println!("  add rax, {}", expect_number(&tokens[pos])?);
+            pos += 1;
+            continue;
+        }
+
+        expect(&tokens[pos], '-')?;
+        pos += 1;
+        println!("  sub rax, {}", expect_number(&tokens[pos])?);
+        pos += 1;
+    }
+
+    Ok(())
+}
+
+/// Reads a token when it is the expected char,
+/// otherwise returns an error.
+fn expect(token: &Token, op: char) -> Result<()> {
+    match token.value {
+        TokenKind::Reserved(kw) if op == kw.chars().next().unwrap() => Ok(()),
+        _ => Err(Error {
+            value: ErrorKind::Error(format!("not '{}'", op).leak()),
+            loc: token.loc,
+        }),
+    }
+}
+
+/// Reads a token when it is a number,
+/// otherwise returns an error.
+fn expect_number(token: &Token) -> Result<u64> {
     match token.value {
         TokenKind::Num(n) => Ok(n),
         _ => {
-            eprintln!("not a number");
-            Err(())
+            eprintln!();
+            Err(Error {
+                value: ErrorKind::Error("not a number"),
+                loc: token.loc,
+            })
         }
     }
 }
 
-fn consume(token: &Token, op: char) -> StdResult<(), ()> {
+/// Read a token when it is the expected char and returns true,
+/// otherwise returns false.
+fn consume(token: &Token, op: char) -> bool {
     match token.value {
-        TokenKind::Num(_) => {
-            eprintln!("not {}", op);
-            Err(())
-        }
-        TokenKind::Reserved(kw) => {
-            if kw.chars().next().unwrap() as char == op {
-                Ok(())
-            } else {
-                Err(())
-            }
-        }
+        TokenKind::Reserved(kw) if kw.chars().next().unwrap() == op => true,
+        _ => false,
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 /// Represents all errros.
-enum Error {
-    ParseError,
-    Eof,
+enum ErrorKind {
+    Error(&'static str),
+}
+
+type Error = Annot<ErrorKind>;
+
+impl Error {
+    /// Shows the error in an understandable way.
+    fn show(&self, input: &str) {
+        let msg = match self.value {
+            ErrorKind::Error(msg) => msg,
+        };
+        eprintln!("{}", input);
+        // inserts '^' at the error point
+        eprintln!("{:spaces_num$}^ {}", "", msg, spaces_num = self.loc.start);
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -114,6 +151,9 @@ enum TokenKind {
     Reserved(&'static str),
     /// Digit token.
     Num(u64),
+    /// EOF.
+    // #HACK: I don't want to use Eof.
+    Eof,
 }
 
 type Token = Annot<TokenKind>;
@@ -135,6 +175,16 @@ impl Token {
             loc: Loc { start, end },
         }
     }
+
+    fn with_eof(input: &[u8]) -> Self {
+        Self {
+            value: TokenKind::Eof,
+            loc: Loc {
+                start: input.len(),
+                end: input.len() + 1,
+            },
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -154,6 +204,8 @@ impl<'a> Tokenizer<'a> {
         }
     }
 
+    /// Tokenizes the input and returns the vector of the tokens.
+    /// When fails, return an error.
     fn tokenize(&mut self) -> Result<Vec<Token>> {
         let mut tokens = vec![];
 
@@ -176,13 +228,24 @@ impl<'a> Tokenizer<'a> {
                     let start = self.pos;
                     tokens.push(Token::with_num(self.str_to_num()?, start, self.pos));
                 }
-                _ => return Err(Error::ParseError),
+                _ => {
+                    return Err(Error {
+                        value: ErrorKind::Error("cannot tokenize"),
+                        loc: Loc {
+                            start: self.pos,
+                            end: self.pos + 1,
+                        },
+                    })
+                }
             }
         }
+        tokens.push(Token::with_eof(self.input));
 
         Ok(tokens)
     }
 
+    /// Converts a str to a number , reads the number and returns it.
+    /// When str starts with a number, returns an error.
     fn str_to_num(&mut self) -> Result<u64> {
         let start = self.pos;
         while self.pos < self.input.len() && self.input[self.pos].is_ascii_digit() {
@@ -190,7 +253,13 @@ impl<'a> Tokenizer<'a> {
         }
 
         if start == self.pos {
-            Err(Error::ParseError)
+            return Err(Error {
+                value: ErrorKind::Error("Should be a number"),
+                loc: Loc {
+                    start: self.pos,
+                    end: self.pos + 1,
+                },
+            });
         } else {
             let n = from_utf8(&self.input[start..self.pos])
                 // This always succeeds becase of construction

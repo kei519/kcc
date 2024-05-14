0a3ba2167744a9e072285c4fe8fb0f83b939e771
K-Enter777 <msgbox777@yahoo.co.jp>
2024-05-14 16:34:28 +0900

    Add Config

---
+++ src/config/test.rs
@@ -0,0 +1,47 @@
+use crate::config::Config;
+
+#[test]
+fn test_no_arg() {
+    assert_eq!(Config::new(Vec::<String>::new()), Err(true));
+}
+
+#[test]
+fn test_one_input() {
+    let config = Config::new(["hoge"]).unwrap();
+    assert_eq!(config.input, "hoge");
+    assert!(config.out_path.is_none());
+}
+
+#[test]
+fn test_two_input() {
+    assert_eq!(Config::new(["hoge", "fuga"]), Err(true));
+}
+
+#[test]
+fn test_no_output() {
+    assert_eq!(Config::new(["", "-o"]), Err(true));
+}
+
+#[test]
+fn test_no_input_with_output() {
+    assert_eq!(Config::new(["-o", "a.out"]), Err(true));
+}
+
+#[test]
+fn test_one_input_with_output() {
+    let config = Config::new(["hoge", "-o", "a.exe"]).unwrap();
+    assert_eq!(config.input, "hoge");
+    assert_eq!(config.out_path.unwrap(), "a.exe");
+}
+
+#[test]
+fn test_concat_output() {
+    let config = Config::new(["hoge", "-oa.out"]).unwrap();
+    assert_eq!(config.input, "hoge");
+    assert_eq!(config.out_path.unwrap(), "a.out");
+}
+
+#[test]
+fn test_two_output() {
+    assert_eq!(Config::new(["-oa.exe", "-o", "hoge"]), Err(true));
+}

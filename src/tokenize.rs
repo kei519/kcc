a016d8d436fb5a312bac8c1bd1b3ae03eafc80b5
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-05-15 15:59:43 +0900

    Add parser and recoginize one number

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -13,10 +13,18 @@ impl Tokenizer {
         Self { input, pos: 0 }
     }
 
-    pub fn tokenize(self) -> Result<Vec<Token>> {
+    pub fn tokenize(mut self) -> Result<Vec<Token>> {
         let mut ret = vec![];
 
         while !self.cur().is_empty() {
+            if self.cur()[0].is_ascii_digit() {
+                let start = self.pos;
+                let num = self.str_to_num();
+                let end = self.pos;
+                ret.push(Token::with_num(num, Loc::range(start, end)));
+                continue;
+            }
+
             return Err(Error::CompileError {
                 message: "unrecoginized token".into(),
                 input: self.input,
@@ -43,10 +51,27 @@ impl Tokenizer {
             false
         }
     }
+
+    /// Converts the byte sequence starting from the current position to a number.
+    ///
+    /// # Remarks
+    ///
+    /// This function assumes that the byte sequence starting with a digit.
+    fn str_to_num(&mut self) -> usize {
+        let mut num = 0;
+        while !self.cur().is_empty() && self.cur()[0].is_ascii_digit() {
+            num = num * 10;
+            num += (self.cur()[0] - b'0') as usize;
+            self.next();
+        }
+        num
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum TokenKind {
+    /// Numeric literal.
+    Num(usize),
     /// Represents the end of file.
     Eof,
 }
@@ -54,6 +79,13 @@ pub enum TokenKind {
 pub type Token = Annot<TokenKind>;
 
 impl Token {
+    pub fn with_num(num: usize, loc: Loc) -> Self {
+        Self {
+            data: TokenKind::Num(num),
+            loc,
+        }
+    }
+
     pub fn with_eof(loc: Loc) -> Self {
         Self {
             data: TokenKind::Eof,

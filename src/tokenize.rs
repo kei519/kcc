3c13445a18c6eda6be286705a37d952bdaa3527a
K-Enter777 <msgbox777@yahoo.co.jp>
2024-05-17 10:33:04 +0900

    Tokenize identifiers

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -3,9 +3,9 @@ mod tests;
 
 use crate::util::{Annot, Error, Loc, Result};
 
-const KW: [&'static str; 20] = [
+const KW: [&'static str; 21] = [
     "==", "!=", "<=", ">=", "+", "-", "*", "/", "(", ")", "<", ">", ";", ",", ".", ":", "[", "]",
-    "{", "}",
+    "{", "}", "=",
 ];
 
 const SEP_KW: [&'static str; 1] = ["return"];
@@ -26,12 +26,14 @@ impl Tokenizer {
         let mut ret = vec![];
 
         'main: while !self.cur().is_empty() {
-            if self.cur()[0].is_ascii_whitespace() {
+            // Skip white spaces.
+            if self.head().is_ascii_whitespace() {
                 self.next();
                 continue;
             }
 
-            if self.cur()[0].is_ascii_digit() {
+            // Numeric literal.
+            if self.head().is_ascii_digit() {
                 let start = self.pos;
                 let num = self.str_to_num();
                 let end = self.pos;
@@ -39,6 +41,7 @@ impl Tokenizer {
                 continue;
             }
 
+            // Reserved words that must be separated by a separator.
             for &kw in &SEP_KW {
                 if self.cur().starts_with(kw.as_bytes())
                     && self.cur()[kw.len()..]
@@ -55,6 +58,7 @@ impl Tokenizer {
                 }
             }
 
+            // Reserved words.
             for &kw in &KW {
                 if self.cur().starts_with(kw.as_bytes()) {
                     let start = self.pos;
@@ -65,6 +69,24 @@ impl Tokenizer {
                 }
             }
 
+            // Identifier.
+            if is_ident_start(self.head()) {
+                let start = self.pos;
+                self.next();
+
+                while !self.cur().is_empty() && is_ident_continue(self.head()) {
+                    self.next();
+                }
+                let end = self.pos;
+
+                ret.push(Token::with_ident(
+                    &self.input[start..end],
+                    Loc::range(start, end),
+                ));
+                continue;
+            }
+
+            // Unrecognized token.
             return Err(Error::CompileError {
                 message: "unrecoginized token".into(),
                 input: self.input,
@@ -79,6 +101,11 @@ impl Tokenizer {
     /// Returns the byte sequence starting from the current position.
     fn cur(&self) -> &'static [u8] {
         &self.input.as_bytes()[self.pos..]
+    }
+
+    /// Returns the byte at the current position.
+    fn head(&self) -> u8 {
+        self.cur()[0]
     }
 
     /// Advances the cursor if the cursor will not exceed the input.
@@ -108,12 +135,24 @@ impl Tokenizer {
     }
 }
 
+/// Returns `true` if the given byte is the start of an identifier.
+fn is_ident_start(c: u8) -> bool {
+    c == b'_' || c.is_ascii_alphabetic()
+}
+
+/// Returns `true` if the given byte is a part of an identifier.
+fn is_ident_continue(c: u8) -> bool {
+    c == b'_' || c.is_ascii_alphanumeric()
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum TokenKind {
     /// Numeric literal.
     Num(usize),
     // Keyword.
     Reserved(&'static str),
+    /// Identifier.
+    Ident(&'static str),
     /// Represents the end of file.
     Eof,
 }
@@ -131,6 +170,13 @@ impl Token {
     pub fn with_reserved(keyword: &'static str, loc: Loc) -> Self {
         Self {
             data: TokenKind::Reserved(keyword),
+            loc,
+        }
+    }
+
+    pub fn with_ident(ident: &'static str, loc: Loc) -> Self {
+        Self {
+            data: TokenKind::Ident(ident),
             loc,
         }
     }
